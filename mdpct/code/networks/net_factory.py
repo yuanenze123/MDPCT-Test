202e99b1199036bf545b80006c4d3564707f61e7
yuanenze <82390734+yuanenze123@users.noreply.github.com>
2023-05-17 23:23:17 +0800

    Add files via upload

---
+++ mdpct/code/networks/net_factory.py
@@ -0,0 +1,15 @@
+from networks.unet import UNet
+from networks.VNet import VNet, MDPCT
+
+def net_factory(net_type="unet", in_chns=1, class_num=4, mode = "train"):
+    if net_type == "unet":
+        net = UNet(in_chns=in_chns, class_num=class_num).cuda()
+    elif net_type == "vnet" and mode == "train":
+        net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
+    elif net_type == "mdpct" and mode == "train":
+        net = MDPCT(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=True).cuda()
+    elif net_type == "vnet" and mode == "test":
+        net = VNet(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
+    elif net_type == "mdpct" and mode == "test":
+        net = MDPCT(n_channels=in_chns, n_classes=class_num, normalization='batchnorm', has_dropout=False).cuda()
+    return net
